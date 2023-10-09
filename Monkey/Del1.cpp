08e9769ca61dc5ad3eb07511eea8105d4638af57
Bal1le <christian.arendt.daniel@gmail.com>
2023-10-09 18:15:22 +0200

    Del 1

---
+++ Monkey/Del1.cpp
@@ -0,0 +1,21 @@
+#include "Array.h"
+
+
+int main() {
+
+	int x, N;
+
+	std::cout << " Indtast x: ";
+	std::cin >> x;
+	std::cout << std::endl << " Indtast N: ";
+	std::cin >> N;
+
+
+	std::cout << std::endl << std::endl;
+
+	Array<double> a2(x,N);
+
+
+
+
+}
